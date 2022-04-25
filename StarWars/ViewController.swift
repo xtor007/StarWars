e047ced5fe28ed693240a7fa859ba5661ec9108a
Anatoliy <tolxpams@gmail.com>
2022-04-25 16:11:47 +0300

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Anatoliy Khramchenko on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
